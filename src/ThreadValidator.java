c0699a366eaf9b1c7253c8ea0b018a59ec815fdd
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 16:35:48 -0300

    FIx: added index to ThreadValidator

--- src/ThreadValidator.java
+++ src/ThreadValidator.java
@@ -1,12 +1,14 @@
 public class ThreadValidator extends Thread {
   private int operation;
+  private int index;
 
-  public ThreadValidator(int operation) {
+  public ThreadValidator(int operation,int index) {
     this.operation = operation;
+    this.index = index;
   }
   
     @Override
     public void run() {
-        Validator.validate(operation);
+		Validator.validate(operation, index);
     }
 }
