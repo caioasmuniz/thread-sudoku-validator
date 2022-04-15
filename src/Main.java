be33e71e50c6e71028a8335d1b00b7673b1dfe5b
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 16:59:49 -0300

    Fix: update formating

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-      ThreadValidator[] workers = new ThreadValidator[27];
-      
-      for (int operation = 0; operation < 3; operation++) 
-        for (int index = 0; index < 9; index++) 
-          workers[operation*3 + index] = new ThreadValidator(operation, index);
-          
-      for (int i = 0; i < workers.length; i++)
-        workers[i].start();
-    }
+  public static void main(String[] args) throws InterruptedException {
+    ThreadValidator[] workers = new ThreadValidator[27];
+
+    for (int operation = 0; operation < 3; operation++)
+      for (int index = 0; index < 9; index++)
+        workers[operation * 3 + index] = new ThreadValidator(operation, index);
+
+    for (int i = 0; i < workers.length; i++)
+      workers[i].start();
+  }
 }
