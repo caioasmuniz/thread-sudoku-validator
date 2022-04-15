4f4d89df91ea76711580484fbc0e69775129e062
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 18:19:07 -0300

    Feature: added explanative text in main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) throws InterruptedException {
     ThreadValidator[][] workers = new ThreadValidator[3][9];
 
+    System.out.println(
+        "O programa indicara todas as linhas/colunas/subgrids invalidos no sudoku. Caso o sistema não aponte nenhuma, o sudoku eh valido.");
+
     for (int operation = 0; operation < 3; operation++)
       for (int index = 0; index < 9; index++)
         workers[operation][index] = new ThreadValidator(operation, index);
