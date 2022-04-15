f6e6f076c97bad728b379c06c8e4a2f759bfed78
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 18:12:11 -0300

    Fix: uncommented main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
   public static void main(String[] args) throws InterruptedException {
-    // ThreadValidator[][] workers = new ThreadValidator[3][9];
-    //
-    // for (int operation = 0; operation < 3; operation++)
-    // for (int index = 0; index < 9; index++)
-    // workers[operation][index] = new ThreadValidator(operation, index);
-    //
-    // for (int operation = 0; operation < 3; operation++)
-    // for (int index = 0; index < 9; index++)
-    // workers[operation][index].start();
-    // ;
+    ThreadValidator[][] workers = new ThreadValidator[3][9];
 
-    ThreadValidator worker = new ThreadValidator(0, 0);
-    worker.start();
+    for (int operation = 0; operation < 3; operation++)
+      for (int index = 0; index < 9; index++)
+        workers[operation][index] = new ThreadValidator(operation, index);
+
+    for (int operation = 0; operation < 3; operation++)
+      for (int index = 0; index < 9; index++)
+        workers[operation][index].start();
   }
 }
