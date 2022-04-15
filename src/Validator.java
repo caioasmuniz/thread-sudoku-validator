8f06181d1cd7d98e27c488a63682e37e98b481c5
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 16:59:23 -0300

    FIx: updated formating

--- src/Validator.java
+++ src/Validator.java
@@ -1,18 +1,18 @@
 public class Validator {
-  private static final int [][] sudoku = {
-    {2, 3, 7, 8, 4, 1, 5, 6, 9},
-    {1, 8, 6, 7, 9, 5, 2, 4, 3},
-    {5, 9, 4, 3, 2, 6, 7, 1, 8},
-    {3, 1, 5, 6, 7, 4, 8, 9, 2},
-    {4, 6, 9, 5, 8, 2, 1, 3, 7},
-    {7, 2, 8, 1, 3, 9, 4, 5, 6},
-    {6, 6, 2, 9, 1, 8, 3, 7, 5},
-    {8, 5, 3, 4, 6, 7, 9, 2, 1},
-    {9, 7, 1, 2, 5, 3, 6, 8, 4},
+  private static final int[][] sudoku = {
+      { 2, 3, 7, 8, 4, 1, 5, 6, 9 },
+      { 1, 8, 6, 7, 9, 5, 2, 4, 3 },
+      { 5, 9, 4, 3, 2, 6, 7, 1, 8 },
+      { 3, 1, 5, 6, 7, 4, 8, 9, 2 },
+      { 4, 6, 9, 5, 8, 2, 1, 3, 7 },
+      { 7, 2, 8, 1, 3, 9, 4, 5, 6 },
+      { 6, 6, 2, 9, 1, 8, 3, 7, 5 },
+      { 8, 5, 3, 4, 6, 7, 9, 2, 1 },
+      { 9, 7, 1, 2, 5, 3, 6, 8, 4 },
   };
   private static boolean valid = true;
-  
-  //checks if each element is "sudoku-valid" (numbers from 1-9 non repeatable)
+
+  // checks if each element is "sudoku-valid" (numbers from 1-9 non repeatable)
   private static boolean validateArray(int array[]) {
     boolean found = false;
     for (int i = 0; i < 9; i++) {
@@ -45,14 +45,14 @@ public class Validator {
     valid = validateArray(column);
   }
 
-// Disposition of the subgrids:
-// 0 1 2
-// 3 4 5
-// 6 7 8
-// Coordinates from each subgrid's first element (top left)
-// sub 0-[0][0]   sub 1-[3][0]    sub 2-[6][0]
-// sub 3-[0][3]   sub 4-[3][3]    sub 5-[6][3]
-// sub 6-[0][6]   sub 7-[3][6]    sub 8-[6][6]
+  // Disposition of the subgrids:
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
+  // Coordinates from each subgrid's first element (top left)
+  // sub 0-[0][0] sub 1-[3][0] sub 2-[6][0]
+  // sub 3-[0][3] sub 4-[3][3] sub 5-[6][3]
+  // sub 6-[0][6] sub 7-[3][6] sub 8-[6][6]
 
   public static void checkSubgrid(int subgridNumber) {
     int[] subgrid = new int[9];
@@ -63,24 +63,27 @@ public class Validator {
     }
     valid = validateArray(subgrid);
   }
-  //validate a Row/Column/Subgrid from the sudoku using the checkRow/Column/Subgrid methods
+
+  // validate a Row/Column/Subgrid from the sudoku using the
+  // checkRow/Column/Subgrid methods
   // operation = 0 checkRow
   // operation = 1 checkColumn
   // operation = 2 checkSubgrid
   public synchronized static void validate(int operation, int index) {
-      switch (operation) {
-        case 0:
-          checkRow(index);
-          break;
-        case 1:
-          checkColumn(index);
-          break;
-        case 2:
-          checkSubgrid(index);
-          break;
-      }
+    switch (operation) {
+      case 0:
+        checkRow(index);
+        break;
+      case 1:
+        checkColumn(index);
+        break;
+      case 2:
+        checkSubgrid(index);
+        break;
     }
-  public static boolean isValid(){
+  }
+
+  public static boolean isValid() {
     return valid;
   }
 }
