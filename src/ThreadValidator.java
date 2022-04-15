28a28c2ce41f4c11f83e9b5a31173dcf5fbe269e
caioasmuniz <caiomuniz888@gmail.com>
2022-04-15 17:00:43 -0300

    Feature: printing results on stdout

--- src/ThreadValidator.java
+++ src/ThreadValidator.java
@@ -2,13 +2,29 @@ public class ThreadValidator extends Thread {
   private int operation;
   private int index;
 
-  public ThreadValidator(int operation,int index) {
+  public ThreadValidator(int operation, int index) {
     this.operation = operation;
     this.index = index;
   }
-  
-    @Override
-    public void run() {
-		Validator.validate(operation, index);
+
+  @Override
+  public void run() {
+    Validator.validate(operation, index);
+    if (Validator.isValid())
+      System.out.print("O sudoku eh valido ");
+    else
+      System.out.print("O sudoku NAO eh valido ");
+    switch (operation) {
+      case 0:
+        System.out.print("na linha ");
+        break;
+      case 1:
+        System.out.print("na coluna ");
+        break;
+      case 3:
+        System.out.print("no subgrid ");
+        break;
     }
+    System.out.print(index + 1);
+  }
 }
